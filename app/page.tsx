64560f2a5696079c9f7f8ee39998c04c360ed957
Naomi <nahteruya@gmail.com>
2024-11-06 07:41:53 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
-    <h1 className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
-      Home Page
-    </h1>
+    <>
+      <h1 className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
+        Home Page
+      </h1>
+      <Button>Enviar</Button>
+    </>
   );
 };
 
